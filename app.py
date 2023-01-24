0837cb8005da4cf0864f0a1c2364ee5e819562e3
Maria.Keshish <mari.black@list.ru>
2023-01-24 17:13:38 +0300

    almost done

--- app.py
+++ app.py
@@ -1,26 +1,22 @@
-from flask import render template, Flask, request 
+from flask import render_template, Flask, request 
 import json
 
-app = Flask(__name__)
+app=Flask(__name__)
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="127.0.0.1", port=5000)
+    app.run(debug=True)
 
 @app.route('/')
 @app.route('/index.html')
 def index():
-    #with open('index.html', 'r') as f:
-     #content = f.read()
-    #return content
     return render_template('index.html')
 
 @app.route('/map.html')
 def map():
-    #with open('map.html', 'r') as f:
-     #content = f.read()
-    #return content
     return render_template('map.html')
 
 @app.route('/women.html')
 def women():
-    #with open('women.html', 'r') as f:
-     #content = f.read()
-    #return content
     return render_template('women.html')
